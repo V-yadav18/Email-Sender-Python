e591874526ad5caa1e8147fd2f4066f6b288e5ae
Vikas Yadav <vikasyadav9138@gmail.com>
2024-01-26 01:09:51 +0530

    Initial

---
+++ send-email-main/send_email.py
@@ -0,0 +1,26 @@
+import smtplib
+
+def send_email(sendto, subject, text):
+    username = "YOUR_GMAIL_EMAIL"  # Change this!
+    app_password = "YOUR_APP_PASSWORD"  # Change this to your App Password
+
+    for i in range(3):
+        try:
+            print("Sending Email to {} (trial {})...".format(sendto, i+1))
+            
+            # Using SMTP_SSL for a secure connection
+            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            
+            server.login(username, app_password)
+            msg = 'Subject: {}\n\n{}'.format(subject, text)
+            server.sendmail(username, sendto, msg)
+            server.quit()
+            
+            print("Email sent!")
+            break
+        except Exception as e:
+            print("Failed to send email due to Exception:")
+            print(e)
+
+# Example usage
+send_email("recipient@example.com", "Test Subject", "This is a test email.")
